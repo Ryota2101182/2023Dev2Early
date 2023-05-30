f7751aed7ffcd6a65018a6eeb4aa71a866ce9cc6
ASO2101231 <2101231@s.asojuku.ac.jp>
2023-05-30 09:55:08 +0900

    Create code.php

---
+++ teamAphp/code.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<meta content="text/html; charset=utf-8" http-equiv="Content-Type">
+<title>webstore</title>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<style>
+.img{
+  width: 300px;
+  height: 300px;
+}
+.container{
+  padding: auto;
+}
+.wrapper{
+  margin: auto;
+  width:auto;
+}
+.textbox{
+  margin:auto;
+}
+
+a.btn--circle {
+  width: 60px;
+  height: 60px;
+  padding: 0;
+  background: #fff;
+  border-radius: 50%;
+  -webkit-box-shadow: 0 3px 5px rgba(0, 0, 0, .3);
+  box-shadow: 0 3px 5px rgba(0, 0, 0, .3);
+}
+a.btn--circle i {
+  font-size: 150%;
+  line-height: 60px;
+  -webkit-transition: all .3s;
+  transition: all .3s;
+  letter-spacing: 0;
+}
+a.btn--circle:hover i {
+  line-height: 55px;
+}
+
+</style>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+<meta name="viewport" content="width=device-width, initial-scale=1" />
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.2/font/bootstrap-icons.css">
+<link rel="stylesheet" href="css/itemstyle.css">
+</head>
+
+<body>
+    <?php require_once 'header.php'?>
+<section class="wrapper">
+  <div class="container">
+    <div class="border col-12 text-center">
+	      <br>
+	      <h2>COMMENT</h2>
+    </div>
+      <div class = "comment">
+        <div class="col-12 text-left">
+          <h5>username</h5>
+        </div>
+        <div class = "col-12 text-center">
+          <textarea cols="80" rows="7"></textarea>
+        </div>
+        <div class="col-12 text-right">
+          <h5>day 4-12</h5>
+        </div>
+        <div class="col-12 text-left">
+          <h5>username</h5>
+        </div>
+        <div class = "col-12 text-center">
+          <textarea cols="80" rows="7"></textarea>
+        </div>
+        <div class="col-12 text-right">
+          <h5>day 4-13</h5>
+        </div>
+        <div class="ucomment">
+          <div class = "col-7 text-center">
+            <textarea cols="65" rows="3"></textarea>
+          </div>
+        </div>
+        <a href="" class="col-3 btn btn--circle btn--circle-c btn--shadow"><i class="fas fa-arrow-up"></i></a>
+      </div>
+  </div>
+</section>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
+</body>
+</html>
+
