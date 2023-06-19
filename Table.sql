60e76a2f41c85e9ff60d7654838b7151c806f572
akieda2101339 <2101339@s.asojuku.ac.jp>
2023-06-19 10:46:46 +0900

    Update Table.sql

--- Table.sql
+++ Table.sql
@@ -9,15 +9,12 @@ CREATE TABLE relation
 CREATE TABLE post
 (post_id INT NOT NULL,
  user_id INT NOT NULL,
- item_id INT NOT NULL,
  post_contents VARCHAR(191) NOT NULL,
  post_image VARCHAR(191) NOT NULL,
  post_date DATE NOT NULL,
  PRIMARY KEY(post_id),
  FOREIGN KEY fkey10(user_id)
-             REFERENCES user(user_id),
- FOREIGN KEY fkey11(item_id)
-             REFERENCES items(item_id)
+             REFERENCES user(user_id)
 )
 
 CREATE TABLE comment
@@ -26,8 +23,20 @@ CREATE TABLE comment
  user_id INT NOT NULL,
  comment_contents VARCHAR(191) NOT NULL,
  comment_date DATE NOT NULL,
- PRIMARY KEY fkey12(post_id)
+ PRIMARY KEY(comment_id),
+ FOREIGN KEY fkey11(post_id)
              REFERENCES post(post_id),
- PRIMARY KEY fkey13(user_id)
+ FOREIGN KEY fkey12(user_id)
             REFERENCES user(user_id)
+)
+
+CREATE TABLE postitem
+(postitem_id INT NOT NULL,
+ post_id INT NOT NULL,
+ item_id INT NOT NULL,
+ PRIMARY KEY(postitem_id),
+ FOREIGN KEY fkey13(post_id)
+             REFERENCES post(post_id),
+ FOREIGN KEY fkey14(item_id)
+             REFERENCES items(item_id)
 )
